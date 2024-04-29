0553c92c346fe5fe0d62a9f20f7ba1b7a40f82fa
ShebinSp <shebinsp@gmail.com>
2024-04-29 17:55:13 +0530

    unit 3

--- strings_runes_bytes_UTF8/string_length/main.go
+++ strings_runes_bytes_UTF8/string_length/main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	// STRING LENGTH IN BYTES AND RUNES
+
+	// Unicode characters occupy between 1 and 4 bytes, ASCII letters occupy 1 byte
+	//  and other unicode code points use more bytes in Go.
 	s1 := "Go"                          // a string used only ASCII
 	fmt.Println("len of s1: ", len(s1)) // 2 each ascii character used 1 byte
 
@@ -14,11 +18,9 @@ func main() {
 
 	fmt.Println(len("ആ")) // 3 bytes
 
-
 	// returns number of runes or unicode points, not bytes
-	n :=utf8.RuneCountInString(word) // 3 runes or characters in the string 'word'. 3 runes * 3 bytes = 9 (len(word))
-	m := utf8.RuneCountInString("ആ") // 1 rune & 3 bytes
-	fmt.Println("n & m: ",n,m)
+	n := utf8.RuneCountInString(word) // 3 runes or characters in the string 'word'. 3 runes * 3 bytes = 9 (len(word))
+	m := utf8.RuneCountInString("ആ")  // 1 rune & 3 bytes
+	fmt.Println("n & m: ", n, m)
 
 }
-
