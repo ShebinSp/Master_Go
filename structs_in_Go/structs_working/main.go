87f4ffc69d95d38f88106794796285dd452b8bb7
ShebinSp <shebinsp@gmail.com>
2024-05-09 14:05:31 +0530

    unit 2

--- structs_in_Go/structs_working/main.go
+++ structs_in_Go/structs_working/main.go
@@ -28,24 +28,19 @@ func main() {
 	lastBook.year = 1878
 	fmt.Printf("last book: %+v\n", lastBook) // z%+v prints both the field names and their values
 
-
 	// How to compare the struct values?
 	// We can compare struct values using double equals sign and not equals.
 	// Two structs are equal if their corresponding fields are equal.
 
-	someBook := book{title:"Anna Karenina"}
-	fmt.Println("someBook equal to aBook?: ",someBook == lastBook)
-	aBook := book {title:"Anna Karenina", author:"Leo Tolstoy", year:1878}
-	fmt.Println("lastBook equal to aBook?: ",aBook == lastBook)
-
+	someBook := book{title: "Anna Karenina"}
+	fmt.Println("someBook equal to aBook?: ", someBook == lastBook)
+	aBook := book{title: "Anna Karenina", author: "Leo Tolstoy", year: 1878}
+	fmt.Println("lastBook equal to aBook?: ", aBook == lastBook)
 
 	// How to copy a struct
 	// When we want to make a copy or a clone of a struct, we can use the equal sign or the assignment operator.
 	// Go will create a brand new struct value in memory.
-	myBook := aBook 
+	myBook := aBook
 	myBook.year = 2022
-	fmt.Println("myBook: ",myBook, "  		   aBook: ",aBook)
-
-
-
+	fmt.Println("myBook: ", myBook, "  		   aBook: ", aBook)
 }
